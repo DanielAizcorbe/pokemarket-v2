401d0537c032844d68c8144752fc549c01282760
Daniel-Aizcorbe <daniel.aizcorbe1@gmail.com>
2024-06-07 19:17:11 -0300

    add titulo principal

---
+++ src/components/pagina-principal/TituloPrincipal.tsx
@@ -0,0 +1,14 @@
+const TituloPrincipal = () => {
+    return (
+        <div className='w-1/2'>
+            <h1 className='text-4xl font-semibold antialiased italic text-center p-3'>
+                ¡Bienvenido entrenador!
+            </h1>
+            <h2 className='text-center text-lg italic'>
+                ¡Tenemos cosas nuevas para vos!
+            </h2>
+        </div>
+    )
+}
+
+export default TituloPrincipal
