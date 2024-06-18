75c5ffaa54e644078a3660e324ed2eb292b93c12
Daniel-Aizcorbe <daniel.aizcorbe1@gmail.com>
2024-06-18 17:09:52 -0300

    remove console.log

--- src/services/pokemon/ids/getPosiblesIds.ts
+++ src/services/pokemon/ids/getPosiblesIds.ts
@@ -5,7 +5,6 @@ import { Atributos, Categoria, MAX_ID_POKEMON } from "app/services/typos";
  export async function getPosiblesIdsDe(atributo: Atributos): Promise<Array<number>> {
 
     const typeData = await getFromPokeApi(Categoria.TYPE_ENDPOINT, atributo);
-    console.log(typeData);
     
     const pokemonUrls = typeData
         .pokemon
